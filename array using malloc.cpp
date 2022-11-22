820a06931cff0ef69fa6d70561a718aaedb1098f
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-22 15:02:39 +0700

    Update array using malloc.cpp

--- array using malloc.cpp
+++ array using malloc.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 main(){
-	int *ptr = (int*) malloc(sizeof(int));
-	*ptr = 2;
-	cout << ptr << endl;
-	cout << *ptr << endl;
+	int *ptr = (int*) malloc(5*sizeof(int));
+	for(int i = 0; i < 5; i++)
+		cin >> ptr[i];
+	for(int i = 0; i < 5; i++){
+		cout << "ptr[" << i << "] = " << ptr[i] << endl;
+	}
 }
