85fc6e074718d47137e1e7aae0080fd71cd4276e
kng-01k <84111662+kng-01k@users.noreply.github.com>
2022-11-22 15:05:47 +0700

    Update array using malloc.cpp

--- array using malloc.cpp
+++ array using malloc.cpp
@@ -6,7 +6,6 @@ main(){
 	int *ptr = (int*) malloc(5*sizeof(int));
 	for(int i = 0; i < 5; i++)
 		cin >> ptr[i];
-	for(int i = 0; i < 5; i++){
+	for(int i = 0; i < 5; i++)
 		cout << "ptr[" << i << "] = " << ptr[i] << ", *(ptr+i) = " << *(ptr+i) << endl;
-	}
 }
